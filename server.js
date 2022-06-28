b1289b106866d788cd68b2c6a0c657edae7ec3fe
HelloAyan <18103373@iubat.edu>
2022-06-28 15:08:39 +0600

    modified server.js file

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 8080
+
 app.get('/', (req, res) =>{
     res.send("CRUD Application"); 
 })
 
-app.listen(3000, ()=>{
-    console.log("Server is running on Port 3000");
+app.listen(PORT, ()=>{
+    console.log(`Server is running on Port ${PORT}`);
 })
