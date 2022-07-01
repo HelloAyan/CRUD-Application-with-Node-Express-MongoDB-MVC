1c6573c391e998724a349420787092e2184de90c
HelloAyan <18103373@iubat.edu>
2022-07-01 17:54:55 +0600

    Route description added

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,11 +3,18 @@ const route = express.Router();
 
 const services =require('../services/render');
 
-
+// @description Root Route
+// @method GET
 route.get('/', services.homeRouters);
 
+
+// @description add-user Route
+// @method GET
 route.get('/add-user', services.addUser);
 
+
+// @description Update-user Route
+// @method GET
 route.get('/update-user', services.updateUser);
 
 module.exports = route;
