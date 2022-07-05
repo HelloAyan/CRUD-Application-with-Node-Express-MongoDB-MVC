76be6bfe7f4dc34cd2fc10eb05d83f09f6ca457a
HelloAyan <18103373@iubat.edu>
2022-07-05 12:56:55 +0600

    Modified axios.get method

--- server/services/render.js
+++ server/services/render.js
@@ -1,8 +1,15 @@
 const axios = require('axios');
 
 exports.homeRouters = (req, res) =>{
-    // Make a get request to /api/users
-    res.render("index.ejs", {user: "New Data"}); 
+    // Make a get request to /api/user
+    axios.get('http://localhost:3000/api/users')
+    .then(function (response){
+        
+        res.render("index.ejs", {user: response.data});
+    })
+    .catch(err =>{
+        res.send(err);
+    })
 }
 
 exports.addUser = (req, res) =>{
